781c35d7971a3643b8d0f15d190a3105f44b72d9
Karolina Krzysik <79480965+KarolinaKrzysik@users.noreply.github.com>
2021-02-28 18:15:47 +0000

    Update wk3ex2action.php

--- wk3ex2action.php
+++ wk3ex2action.php
@@ -9,6 +9,6 @@
         case "Male": echo "You are a male."; break;
         case "Female": echo "You are a female."; break;
         case "Other": echo "You identify yourself as neither male nor female."; break;
-        default: echo "Your input wasn\'t in a required format."; 
+        default: echo "Your input wasn't in a required format."; 
     }
 ?>
