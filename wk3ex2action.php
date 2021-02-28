c0d295ce20809f8661df61f347973a49e9df369c
Karolina Krzysik <79480965+KarolinaKrzysik@users.noreply.github.com>
2021-02-28 18:09:45 +0000

    Week 3 task 2 PHP file another textbox. Switch

--- wk3ex2action.php
+++ wk3ex2action.php
@@ -5,4 +5,10 @@
     else{
         echo "You are 21 years old or over. <br/>";
     }
+    switch ($_POST["txtgender"]){
+        case "Male": echo "You are a male."; break;
+        case "Female": echo "You are a female."; break;
+        case "Other": echo "You identify yourself as neither male nor female."; break;
+        default: echo "Your input wasn\'t in a required format."; 
+    }
 ?>
