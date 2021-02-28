1cfd28b84ee4432c614f369949c419364c6d2cc1
Karolina Krzysik <79480965+KarolinaKrzysik@users.noreply.github.com>
2021-02-28 19:02:47 +0000

    Week 3 task 3 Add new course

--- wk3ex3action.php
+++ wk3ex3action.php
@@ -3,6 +3,7 @@
         case "HNC": echo "You have selected HNC course."; break;
         case "HND": echo "You have selected HND course."; break;
         case "BSC": echo "You have selected BSC course."; break;
+        case "PartTimeBSC": echo "You have selected Part Time BSC course."; break;
         default: echo "No course selected.";
     }
 ?>
